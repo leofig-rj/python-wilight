9feb1b2a46ee94ebb6f6814ea15b056fedceb8bb
Leonardo Figueiro <leoagfig@gmail.com>
2020-05-01 00:16:05 -0300

    Version 0.0.28

--- wilight/protocol.py
+++ wilight/protocol.py
@@ -92,7 +92,7 @@ class WiLightProtocol(asyncio.Protocol):
         #self.logger.warning('handle data: %s', packet)
         if packet[0:1] == b'&':
             if self.client.model == "0102":
-                _handle_0102_packet(self, packet)
+                self._handle_0102_packet(self, packet)
         else:
             self.logger.warning('received unknown packet: %s', packet)
 
